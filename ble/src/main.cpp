d386fd9c7d4aec3fe488922e97f8831ad33e48a1
Shiv Patel <shivpat456@gmail.com>
2025-10-28 21:48:03 -0700

    Working BLE that is visible on my Phone

--- ble/src/main.cpp
+++ ble/src/main.cpp
@@ -4,28 +4,22 @@
 #include <BLEUtils.h>
 #include <BLE2902.h>
 
-BLECharacteristic *pCharacteristic;
-bool deviceConnected = false;
-const char* SERVICE_UUID = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
-const char* CHARACTERISTIC_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8";
-
-class MyServerCallbacks: public BLEServerCallbacks {
-  void onConnect(BLEServer* pServer) { deviceConnected = true; }
-  void onDisconnect(BLEServer* pServer) { deviceConnected = false; }
-};
+#define CHARACTERISTIC_UUID "61af0d7c-6335-41de-af2f-360dd9282593"
+#define SERVICE_UUID  "ec93578b-ca2e-48f6-b773-7db669768072"
+#define DEVICE_NAME  "devlosisgay"
 
 void setup() {
-  Serial.begin(115200);
-  BLEDevice::init("XIAO-ESP32C3");
+  Serial.begin(115200);  
+  BLEDevice::init(DEVICE_NAME);
+  BLEServer* pServer = BLEDevice::createServer(); //chunk of memory allocated to create a server
+  BLEService* pService = pServer->createService(SERVICE_UUID);  // Profile -> Service -> Characterisits GATT Architecture
+  BLECharacteristic* pCharateristic = pService->createCharacteristic(CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ); // just read properties
+  // find device
+  pCharateristic->setValue("shanti");
+  BLEAdvertising *pAdvertising = pServer->getAdvertising();
   pAdvertising->start();
-  Serial.println("waiting for a connection.. ");
-  
 }
 
 void loop() {
-  if (deviceConnected) {
-    pCharacteristic->setValue("Hello from Xiao ESP32C3");
-    pCharacteristic->notify();
-    delay(1000);
-  }
+  delay(2000);
 }
