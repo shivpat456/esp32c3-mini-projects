1e79f4bc0c64e4feedd5551367f7e60ee40eb1e9
Shiv Patel <shivpat456@gmail.com>
2025-11-02 16:09:16 -0800

    working: prints internal temp via BLE to my phone

--- ble/src/main.cpp
+++ ble/src/main.cpp
@@ -4,22 +4,31 @@
 #include <BLEUtils.h>
 #include <BLE2902.h>
 
-#define CHARACTERISTIC_UUID "61af0d7c-6335-41de-af2f-360dd9282593"
-#define SERVICE_UUID  "ec93578b-ca2e-48f6-b773-7db669768072"
-#define DEVICE_NAME  "devlosisgay"
+#define CHARACTERISTIC_UUID "2A1C" // Temperature Measurment   
+#define SERVICE_UUID "181A"  
+#define DEVICE_NAME  "Shanti"
+#define ASSIGNED_NUM 543 // Temperature Reading
 
+BLECharacteristic* pCharactheristic;
 void setup() {
   Serial.begin(115200);  
   BLEDevice::init(DEVICE_NAME);
   BLEServer* pServer = BLEDevice::createServer(); //chunk of memory allocated to create a server
   BLEService* pService = pServer->createService(SERVICE_UUID);  // Profile -> Service -> Characterisits GATT Architecture
-  BLECharacteristic* pCharateristic = pService->createCharacteristic(CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ); // just read properties
+  pCharactheristic = pService->createCharacteristic(CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY); // just read properties
+  pCharactheristic->addDescriptor(new BLE2902());
+  pService->start();
   // find device
-  pCharateristic->setValue("shanti");
   BLEAdvertising *pAdvertising = pServer->getAdvertising();
+  pAdvertising->setAppearance(ASSIGNED_NUM);
   pAdvertising->start();
 }
 
 void loop() {
   delay(2000);
+  float internalTemp = temperatureRead();
+  std::string message = "The internal temperature is: " + std::to_string(internalTemp);
+  pCharactheristic->setValue(message);
+  pCharactheristic->notify();
+  Serial.print("The value is working");
 }
