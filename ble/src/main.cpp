52c360169fa26b17da4d4dccaf1da006a29043a5
Shiv Patel <shivpat456@gmail.com>
2025-11-02 16:53:26 -0800

    Save Energy Using Callbacks; Only Displays when Connected

--- ble/src/main.cpp
+++ ble/src/main.cpp
@@ -10,10 +10,28 @@
 #define ASSIGNED_NUM 543 // Temperature Reading
 
 BLECharacteristic* pCharactheristic;
+BLEServer* pServer;
+
+
+bool deviceConnected = false;
+
+class myCallBacks : public BLEServerCallbacks {
+  void onConnect(BLEServer* pServer){
+    Serial.print("Phone is Connected");
+    deviceConnected = true;
+  }
+  void onDisconnect(BLEServer* pServer) {
+    Serial.print("Phone disconnected");
+    deviceConnected = false;
+    (pServer->getAdvertising())->start();
+  }
+};
+
 void setup() {
   Serial.begin(115200);  
   BLEDevice::init(DEVICE_NAME);
-  BLEServer* pServer = BLEDevice::createServer(); //chunk of memory allocated to create a server
+  pServer = BLEDevice::createServer(); //chunk of memory allocated to create a server
+  pServer->setCallbacks(new myCallBacks());
   BLEService* pService = pServer->createService(SERVICE_UUID);  // Profile -> Service -> Characterisits GATT Architecture
   pCharactheristic = pService->createCharacteristic(CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY); // just read properties
   pCharactheristic->addDescriptor(new BLE2902());
@@ -26,9 +44,13 @@ void setup() {
 
 void loop() {
   delay(2000);
-  float internalTemp = temperatureRead();
-  std::string message = "The internal temperature is: " + std::to_string(internalTemp);
-  pCharactheristic->setValue(message);
-  pCharactheristic->notify();
-  Serial.print("The value is working");
+  if (deviceConnected) {  
+    float internalTemp = temperatureRead();
+    std::string message = "The internal temperature is: " + std::to_string(internalTemp);
+    pCharactheristic->setValue(message);
+    pCharactheristic->notify();
+    Serial.print("The value is working");
+  } else {
+    Serial.println("Device is Disconnected");
+  }
 }
